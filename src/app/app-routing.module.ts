37620326c2fea89aef617973b1789903b838bdd6
= <=>
2022-08-03 11:17:25 +0530

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostDashboardComponent } from './post-dashboard/post-dashboard.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'signin' , component : SigninComponent},
+  {path : 'signup', component : SignupComponent},
+  {path : 'dashboard' , component : PostDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
