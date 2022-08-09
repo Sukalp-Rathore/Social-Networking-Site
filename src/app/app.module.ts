b98c9cb85c8437788523081c9b5c86e0dd38ab6a
= <=>
2022-08-09 11:23:23 +0530

    create post done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { PostDashboardComponent } from './post-dashboard/post-dashboard.component';
-import {HttpClientModule} from '@angular/common/http';
-import { WelcomeComponent } from './welcome/welcome.component'
-import {AngularFireModule} from '@angular/fire/compat'
+import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CreatepostsComponent } from './createposts/createposts.component';
 
 @NgModule({
   declarations: [
@@ -22,19 +23,20 @@ import { HomepageComponent } from './homepage/homepage.component';
     SignupComponent,
     PostDashboardComponent,
     WelcomeComponent,
-    HomepageComponent
-  ],  
+    HomepageComponent,
+    CreatepostsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase ),
+    AngularFireModule.initializeApp(environment.firebase),
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatDialogModule,
-    MatIconModule  
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/createposts/createposts.component.ts
+++ src/app/createposts/createposts.component.ts
@@ -4,7 +4,14 @@ import {
   HttpClientModule,
   HttpHeaders,
 } from '@angular/common/http';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Route, Router } from '@angular/router';
 
 @Component({
@@ -25,18 +32,17 @@ export class CreatepostsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.createpostForm = this.formbuilder.group({
-      text: '',
-      image:
-        'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_960_720.jpg',
-      likes: 0,
-      tags: 'notags',
-      owner: '',
-    });
-
     let getUserData = JSON.parse(
       localStorage.getItem('userData') || 'no user founds'
     );
+    this.createpostForm = this.formbuilder.group({
+      text: '',
+      image: 'www.images.com',
+      likes: 0,
+      tags: '',
+      owner: getUserData.id,
+    });
+
     console.log('user id ' + getUserData.id);
     console.log('user fName ' + getUserData.firstName);
     console.log('user lName ' + getUserData.lastName);
@@ -45,29 +51,18 @@ export class CreatepostsComponent implements OnInit {
     this.id = getUserData.id;
   }
   onPostClick() {
-    console.log(this.createpostForm.get('text')?.value);
+    console.log('=========>', this.createpostForm);
+
     let httpHeaders = new HttpHeaders({ 'app-id': '62ea20e14641344f73b0b152' });
     let url = 'https://dummyapi.io/data/v1/post/create';
     try {
       console.log(this.createpostForm);
       this.http
-        .post<any>(
-          url,
-          {
-            text: this.createpostForm.get('text')?.value,
-            image: 'wwe.googenvhgbhbnhl.come',
-            likes: 0,
-            tags: ['sudo ', 'player', 'modi ji'],
-            owner: this.id,
-          },
-          { headers: httpHeaders }
-        )
+        .post<any>(url, this.createpostForm.value, { headers: httpHeaders })
         .subscribe((res) => {
           console.log(res);
           console.log('data sent successfulll ======>');
           alert('Post Done');
-          // this.createpostForm.reset();
-          // this.router.navigate(['dashboard']);
         });
     } catch (err: any) {
       console.log(err.message);
